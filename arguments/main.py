687597b0c450fbebf46ae59cf098016931a25949
nynkebosveld <98457004+nynkebosveld@users.noreply.github.com>
2022-08-13 12:01:43 +0200

    Arguments opdracht
    opdracht inleveren

---
+++ arguments/main.py
@@ -0,0 +1,40 @@
+# Do not modify these lines
+__winc_id__ = '7b9401ad7f544be2a23321292dd61cb6'
+__human_name__ = 'arguments'
+
+# Add your code after this line
+# Part 1
+def greet(name, greeting= "Hello,"):
+    greeting = greeting + " %s!"
+    return greeting%(name)
+
+print(greet("Robin", "sup"))
+
+# Part 2
+def force(mass, x="earth"):
+
+
+    body = {
+        'sun': 274,
+        "jupiter": 24.9,
+        "neptune": 11.2,
+        "saturn": 10.4,
+        "earth": 9.8,
+        "uranus": 8.9,
+        "venus": 8.9,
+        "mars": 3.7,
+        "mercury": 3.7,
+        "moon": 1.6,
+        "pluto": 0.6
+    }
+
+    return (mass * body.get(x))
+
+print(force(3, "earth"))
+
+# Part 3
+def pull(m1, m2, d):
+    x = round((6.674*10**-11) * ((m1 * m2)/d**2), 10)
+    return x
+
+print(pull(800, 1500, 3))
