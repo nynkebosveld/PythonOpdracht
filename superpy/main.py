89435e1390eb3b6886b372865c486f707b68b89a
nynkebosveld <98457004+nynkebosveld@users.noreply.github.com>
2023-01-09 20:04:57 +0100

    Update main.py

--- superpy/main.py
+++ superpy/main.py
@@ -4,6 +4,8 @@ import json
 import os
 from datetime import datetime, timedelta
 
+__winc_id__ = "a2bc36ea784242e4989deb157d527ba0"
+__human_name__ = "superpy"
 
 def buy(args):
     f = open("bought.csv", "a", newline="")
